aa1bbcb855d1ce3dd569e6d09dffe210a68892f0
Emanuel Tesar <e.tesarr@gmail.com>
2020-08-14 09:52:59 +0200

    Try unsafe React interpolation

--- components/index.tsx
+++ components/index.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Index = () => {
+  const [state, setState] = useState({
+    iframe: '<img src=. onerror=console.log("frame") />',
+    showIframe: false,
+  })
+  const { iframe, showIframe } = state
+
   return (
     <>
       <h1>Hi, </h1>
-      <div>I am dynamically loaded page, executed on client side</div>
+
+      <div>Set iframe srcDoc content</div>
+      <input
+        style={{ width: 300 }}
+        type="text"
+        value={iframe}
+        onChange={(e) => setState({ ...state, iframe: e.target.value })}
+      />
+      <button onClick={() => setState({ ...state, showIframe: !showIframe })}>
+        Toggle iframe with custom content
+      </button>
+      {showIframe && <iframe srcDoc={iframe} />}
     </>
   )
 }
